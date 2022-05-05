210831e5979a8067fd0b4a72b17f3a8f25528f5f
sao.buivan <buivansaobg@gmail.com>
2022-05-05 17:36:56 +0700

    05052022

---
+++ file_input_stream/src/main/java/sds/FileMerge.java
@@ -0,0 +1,2 @@
+package sds;public class FileMerge {
+}
---
+++ test-552022/src/main/java/sds/App.java
@@ -0,0 +1,31 @@
+package sds;
+
+public class App {
+    public static void main(String[] args) {
+        RandomStringGenerator randomStringGenerator1 = new RandomStringGenerator(0, 20);
+        System.out.println(randomStringGenerator1.generate());
+
+        RandomStringGenerator randomStringGenerator2 = new RandomStringGenerator(0, 20);
+        System.out.println(randomStringGenerator2.generate(5));
+
+
+        System.exit(1);
+
+        System.out.println("Array");
+        DistinctCounterNumber array = new DistinctCounterNumber(new int[]{1, 2, 3, 1, 2});
+        System.out.println("Distinct counter: " + array.countDistinct());
+
+        System.out.println("--------------");
+
+        System.out.println("String");
+        DistinctCounterNumber string = new DistinctCounterNumber("1;2;3;1;2");
+        System.out.println("Distinct counter: " + string.countDistinct());
+
+        System.out.println("--------------");
+
+        System.out.println("Input");
+        DistinctCounterNumber input = new DistinctCounterNumber();
+        input.readArray();
+        System.out.println("Distinct counter: " + input.countDistinct());
+    }
+}
